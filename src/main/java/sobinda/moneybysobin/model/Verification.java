25e8b9ccb55807ff166290daf0dbdd0dcafbb04a
Dmitrii Sobin <neo7976@yandex.ru>
2022-12-19 15:07:54 +0300

    confirmOperation - описание шагов

--- src/main/java/sobinda/moneybysobin/model/Verification.java
+++ src/main/java/sobinda/moneybysobin/model/Verification.java
@@ -5,13 +5,15 @@ import javax.validation.constraints.NotNull;
 public class Verification {
     @NotNull
     private String code;
+    private String operationId;
 
     public Verification() {
 
     }
 
-    public Verification(String code) {
+    public Verification(String code, String operationId) {
         this.code = code;
+        this.operationId = operationId;
     }
 
     public String getCode() {
@@ -22,8 +24,17 @@ public class Verification {
         this.code = code;
     }
 
+    public String getOperationId() {
+        return operationId;
+    }
+
+    public void setOperationId(String operationId) {
+        this.operationId = operationId;
+    }
+
     @Override
     public String toString() {
-        return "Секретный код: " + code;
+        return "Секретный код: " + code +
+                "Операция: " + operationId;
     }
 }
