3e3de8c3f24d2834b6a47689fb85598b73150cf6
Dmitrii Sobin <neo7976@yandex.ru>
2022-12-20 12:16:39 +0300

    transferMoneyCardToCardTest - успешно написан

--- src/test/java/sobinda/moneybysobin/repository/TransferRepositoryTest.java
+++ src/test/java/sobinda/moneybysobin/repository/TransferRepositoryTest.java
@@ -1,29 +1,31 @@
 package sobinda.moneybysobin.repository;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import sobinda.moneybysobin.exceptions.InvalidTransactionExceptions;
 import sobinda.moneybysobin.model.Amount;
 import sobinda.moneybysobin.model.Card;
+import sobinda.moneybysobin.model.Verification;
 
 import java.util.stream.Stream;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class TransferRepositoryTest {
     TransferRepository transferRepository;
     public static Card card1 = new Card(
-            "4558 4458 8558 4747",
+            "4558445885584747",
             "08/23",
             "351");
 
     public static Card card2 = new Card(
-            "4558 4458 8558 5555",
+            "4558445885585555",
             "08/23",
             "352");
+
 
     @BeforeEach
     void setUp() {
@@ -38,18 +40,22 @@ class TransferRepositoryTest {
     public static Stream<Arguments> sourceTransfer() {
         return Stream.of(
                 Arguments.of(card1,
-                        "4558 4458 8558 5555",
-                        new Amount(5_000, "RUR")),
+                        card2.getCardNumber(),
+                        new Amount(500_00, "RUR"), "1", new Verification("0000", "1")),
                 Arguments.of(card2,
-                        "4558 4458 8558 4747",
-                        new Amount(10_000, "RUR"))
+                        card1.getCardNumber(),
+                        new Amount(1000_00, "RUR"), "3", new Verification("0000", "3"))
         );
     }
 
     @ParameterizedTest
     @MethodSource("sourceTransfer")
-    void transferMoneyCardToCard(Card cardFrom, String cardNumberTo, Amount amount) throws InvalidTransactionExceptions {
-        transferRepository.transferMoneyCardToCard(cardFrom, cardNumberTo, amount);
-        //todo дописать тест, как будет понятно, что мы возвращаем в конечном итоге
+    void transferMoneyCardToCardTest(Card cardFrom, String cardNumberTo, Amount amount, String number, Verification verification) throws InvalidTransactionExceptions {
+        Assertions.assertEquals("Ожидаем подтверждение на перевод операции №" + number,
+                transferRepository.transferMoneyCardToCard(cardFrom, cardNumberTo, amount));
+        Assertions.assertEquals("Успешная транзакция №" + verification.getOperationId(), transferRepository.confirmOperation(verification));
     }
+//
+//    @Test
+//    void
 }
