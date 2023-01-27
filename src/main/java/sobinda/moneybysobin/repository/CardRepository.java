a83fc98897c1e15583214e555df8710635eeea4a
Dmitrii Sobin <neo7976@yandex.ru>
2023-01-27 15:08:53 +0300

    Операция производится

--- src/main/java/sobinda/moneybysobin/repository/CardRepository.java
+++ src/main/java/sobinda/moneybysobin/repository/CardRepository.java
@@ -1,13 +1,16 @@
 package sobinda.moneybysobin.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import sobinda.moneybysobin.entity.Card;
+import sobinda.moneybysobin.entity.Operation;
 
+import javax.transaction.Transactional;
 import java.math.BigDecimal;
 import java.util.Optional;
-
+@Transactional
 public interface CardRepository extends JpaRepository<Card, Integer> {
 
     Optional<Card> findByCardNumber(String cardNumber);
@@ -21,8 +24,11 @@ public interface CardRepository extends JpaRepository<Card, Integer> {
     @Query(value = "select c.amount.value from Card c where c.cardNumber= :card")
     Optional<BigDecimal> findByCardNumberBalance();
 
+
+    @Modifying
     @Query(value = "update money_transfer.cards c set c.value=:value where c.number=:number", nativeQuery = true)
     void setBalanceCard(@Param("value") BigDecimal bigDecimal,
-                        @Param("number") String number);
+                                       @Param("number") String number);
+
 
 }
