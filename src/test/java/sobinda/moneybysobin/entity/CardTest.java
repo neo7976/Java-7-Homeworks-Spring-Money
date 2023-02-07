a252ff6bcd5b6bb11edcacb218bed27f782cb00a
Dmitrii Sobin <neo7976@yandex.ru>
2023-02-07 14:04:52 +0300

    Начало разработки тестирования нашей БД

--- src/test/java/sobinda/moneybysobin/entity/CardTest.java
+++ src/test/java/sobinda/moneybysobin/entity/CardTest.java
@@ -1,7 +1,23 @@
 package sobinda.moneybysobin.entity;
 
+import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
+import org.junit.Before;
+import sobinda.moneybysobin.config.DBUnitConfig;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class CardTest {
+class CardTest extends DBUnitConfig {
+    private EntityManager entityManager = Persistence.createEntityManagerFactory("DBUnitEx").createEntityManager();
 
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    public CardTest(String name) {
+        super(name);
+    }
 }
