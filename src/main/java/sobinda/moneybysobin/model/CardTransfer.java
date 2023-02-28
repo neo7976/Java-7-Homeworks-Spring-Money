def0c2f82135745c89a38ccdfa72dc291edba77c
Dmitrii Sobin <neo7976@yandex.ru>
2023-02-28 10:03:06 +0300

    CardTransfer - рефакторинг через аннотации

--- src/main/java/sobinda/moneybysobin/model/CardTransfer.java
+++ src/main/java/sobinda/moneybysobin/model/CardTransfer.java
@@ -1,15 +1,17 @@
 package sobinda.moneybysobin.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import sobinda.moneybysobin.entity.Amount;
 
 import javax.validation.constraints.NotNull;
 
-//@JsonIgnoreProperties(ignoreUnknown = true)
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CardTransfer {
     @NotNull
-//    @JsonProperty("Номер карты:")
     private String cardFromNumber;
     @NotNull
     private String cardFromValidTill;
@@ -18,56 +20,4 @@ public class CardTransfer {
     @NotNull
     private String cardToNumber;
     private Amount amount;
-
-
-    public CardTransfer(String cardFromNumber, String cardFromValidTill, String cardFromCVV, String cardToNumber, Amount amount) {
-        this.cardFromNumber = cardFromNumber;
-        this.cardFromValidTill = cardFromValidTill;
-        this.cardFromCVV = cardFromCVV;
-        this.cardToNumber = cardToNumber;
-        this.amount = amount;
-    }
-
-    public CardTransfer() {
-    }
-
-//    public String getCardFromNumber() {
-//        return cardFromNumber;
-//    }
-//
-//    public void setCardFromNumber(String cardFromNumber) {
-//        this.cardFromNumber = cardFromNumber;
-//    }
-//
-//    public String getCardFromValidTill() {
-//        return cardFromValidTill;
-//    }
-//
-//    public void setCardFromValidTill(String cardFromValidTill) {
-//        this.cardFromValidTill = cardFromValidTill;
-//    }
-//
-//    public String getCardFromCVV() {
-//        return cardFromCVV;
-//    }
-//
-//    public void setCardFromCVV(String cardFromCVV) {
-//        this.cardFromCVV = cardFromCVV;
-//    }
-//
-//    public String getCardToNumber() {
-//        return cardToNumber;
-//    }
-//
-//    public void setCardToNumber(String cardToNumber) {
-//        this.cardToNumber = cardToNumber;
-//    }
-//
-//    public Amount getAmount() {
-//        return amount;
-//    }
-//
-//    public void setAmount(Amount amount) {
-//        this.amount = amount;
-//    }
 }
